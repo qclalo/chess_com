35b71015e094d8dcec90d61f61fea973621b420f
selenehienclone <vaporflyb@gmail.com>
2024-06-15 17:14:14 -0400

    failure

--- content.js
+++ content.js
@@ -1,22 +1,22 @@
-function addTitleLinkToUserTagline(username) {
+function addTitleLinkToUserTagline() {
     // Select all div elements with the specified class
-    const userTaglines = document.querySelectorAll('div.user-tagline-component');
-    
+    const userTaglines = document.querySelectorAll('div.user-tagline-popover-component.archived-games-user-info');
+
     // Iterate through each div element
     userTaglines.forEach(userTagline => {
         // Select the <a> element with the specified class and data-test-element attribute inside the div
         const userUsername = userTagline.querySelector('a.user-username-component.user-username-white.user-username-link.user-tagline-username[data-test-element="user-tagline-username"]');
-        
+
         // Check if the <a> element contains the text 'cbrahFor'
-        if (userUsername && userUsername.textContent.trim() === username) {
+        if (userUsername && userUsername.textContent.trim() === 'cbrahFor') {
             // Create the new <a> element
             const titleLink = document.createElement('a');
             titleLink.className = 'user-chess-title-component';
             titleLink.href = '/members/titled-players';
             titleLink.target = '_blank';
-            titleLink.setAttribute('data-tooltip-target', '11');
-            titleLink.innerHTML = 'WNM';
-            
+            titleLink.setAttribute('data-tooltip-target', '709');
+            titleLink.innerHTML = 'GM';
+
             // Insert the <a> element into the <div> element as the first child
             userTagline.insertBefore(titleLink, userTagline.firstChild);
         }
@@ -24,4 +24,4 @@ function addTitleLinkToUserTagline(username) {
 }
 
 // Call the function to add the title links
-addTitleLinkToUserTagline("cbrahFor");
+addTitleLinkToUserTagline();
