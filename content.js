60f898b99b57a465d688b530d093f051048371ae
selenehienclone <vaporflyb@gmail.com>
2024-07-04 14:31:20 -0400

    reformat code

--- content.js
+++ content.js
@@ -1,22 +1,23 @@
+/*
+Iterate through each div element
+Select the <a> element with the specified class and data-test-element attribute inside the div
+Check if the <a> element contains the username text. Create the new <a> element and
+Insert the <a> element into the <div> element as the first child
+*/
+
 function addTitleLinkToUserTagline() {
-    let username = "cbrahFor";
+    let username = "Uarekomodo9";
     let title = "IM";
     // Select all div elements with the specified class
     const userTaglines = document.querySelectorAll(
         'div.user-tagline-component'
     );
 
-    // Iterate through each div element
     userTaglines.forEach(userTagline => {
-        // Select the <a> element with the specified class and data-test-element attribute inside the div
         const userUsername = userTagline.querySelector(
             'a.user-username-component.user-username-white.user-username-link.' +
             'user-tagline-username[data-test-element="user-tagline-username"]'
         );
-
-        // Check if the <a> element contains the username text
-        // Create the new <a> element
-        // Insert the <a> element into the <div> element as the first child
 
         if (userUsername && userUsername.textContent.trim() === username) {
             const titleLink = document.createElement('a');
